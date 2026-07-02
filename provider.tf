808ba3fc1c466216b314d2082e96002de1d2264c
adi8090 <adityagupta8090@gmail.com>
2026-07-02 13:44:00 +0530

    Add AWS provider and OIDC GitHub Actions workflow

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # Feel free to change this to your preferred region (e.g., ap-south-1)
+}
