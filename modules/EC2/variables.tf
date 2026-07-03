e4c018d37ae69589553740049b6d0c581a4fd0f5
adi8090 <adityagupta8090@gmail.com>
2026-07-03 20:29:11 +0530

    EC2 variable update

--- modules/EC2/variables.tf
+++ modules/EC2/variables.tf
@@ -1,3 +1,25 @@
+variable "subnet_id" {
+  description = "The ID of the subnet to deploy the instance in"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "The ID of the security group to attach"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "ubuntu-web-server"
+}
+
 variable "vm_username" {
   description = "The username for the EC2 instance"
   type        = string
@@ -7,5 +29,5 @@ variable "vm_username" {
 variable "vm_password" {
   description = "The SSH password for the EC2 instance"
   type        = string
-  sensitive   = true # This prevents Terraform from printing it in the logs
+  sensitive   = true 
 }
