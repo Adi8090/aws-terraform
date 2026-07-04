3fbb8e41e73d1f2eff2094aa90d8ca9a3d0ecbaa
adi8090 <adityagupta8090@gmail.com>
2026-07-05 02:47:12 +0530

    update ami and t3

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "ec2" {
   subnet_id         = module.subnet.subnet_id
   security_group_id = module.security_group.sg_id
   
-  instance_type     = "t4g.micro" 
+  instance_type     = "t3.micro" 
   
   instance_name     = "ubuntu-web-server"
   vm_password       = var.vm_password 
