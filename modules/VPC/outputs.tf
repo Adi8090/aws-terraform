2889bbad1ea1c7fee3bc2cdc76e32ffa1607d9cf
adi8090 <adityagupta8090@gmail.com>
2026-07-12 06:44:33 +0530

    let see2

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+# <<<<<<< HEAD
+# # output "vpc_id" {
+# #   description = "The ID of the VPC"
+# #   value       = aws_vpc.this.id
+# # }
+
+# =======
 # output "vpc_id" {
-#   description = "The ID of the VPC"
+#   # description = "The ID of the VPC"
 #   value       = aws_vpc.this.id
 # }
-
-=======
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
-}
->>>>>>> parent of a8790d1 (VPC error resolve1)
+# >>>>>>> parent of a8790d1 (VPC error resolve1)
--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -11,7 +11,7 @@ output "subnet_id" {
   value       = aws_subnet.this.id
 }
 
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
-}
+# output "vpc_id" {
+#   description = "The ID of the VPC"
+#   value       = aws_vpc.this.id
+# }
