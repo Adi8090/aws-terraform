44ddf663e44a665446a90d63f50871dc843aae33
adi8090 <adityagupta8090@gmail.com>
2026-07-12 19:27:33 +0530

    resolve subnet issue1

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -1,12 +1,4 @@
-# <<<<<<< HEAD
-# # output "vpc_id" {
-# #   description = "The ID of the VPC"
-# #   value       = aws_vpc.this.id
-# # }
-
-# =======
-# output "vpc_id" {
-#   # description = "The ID of the VPC"
-#   value       = aws_vpc.this.id
-# }
-# >>>>>>> parent of a8790d1 (VPC error resolve1)
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
