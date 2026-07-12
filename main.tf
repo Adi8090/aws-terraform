d58add0e62971276a85e7f3460f7b6ec72843edb
adi8090 <adityagupta8090@gmail.com>
2026-07-12 19:18:21 +0530

    resolve subnet issue

--- main.tf
+++ main.tf
@@ -3,32 +3,27 @@ module "vpc" {
   # variables...
 }
 
-module "subnet" {
-  source = "./modules/subnet"
-  
-  # This is how the subnet knows which VPC to attach to!
-  vpc_id = module.vpc.vpc_id
-}
-
+# 1. We combined your two subnet blocks into this single block
 module "subnet" {
   source      = "./modules/subnet"
-  vpc_id      = module.vpc.vpc_id # Pulling the ID from the VPC module output
+  vpc_id      = module.vpc.vpc_id 
   subnet_cidr = "10.0.1.0/24"
 }
 
+# 2. Security Group (Note: it is highly recommended to rename your folder to "security_group" without a space!)
 module "security_group" {
-  source  = "./modules/security group" # (Update this if you remove the space in the folder name)
+  source  = "./modules/security group" 
   vpc_id  = module.vpc.vpc_id
   sg_name = "web-sg"
 }
 
+# 3. EC2 Instance
 module "ec2" {
   source            = "./modules/EC2"
   subnet_id         = module.subnet.subnet_id
   security_group_id = module.security_group.sg_id
   
   instance_type     = "t3.micro" 
-  
   instance_name     = "ubuntu-web-server"
   vm_password       = var.vm_password 
 }
